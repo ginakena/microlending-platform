0b9fd4e8e72e5688a80b5afeb6d76a0db0edbdcf
ginakena <makenareginam@gmail.com>
2026-01-25 14:47:41 +0300

    added mobile hamburger menu

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
